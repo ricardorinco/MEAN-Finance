ac7b3250ceb3db5ed517b8dc5b1bb3aedc86d06e
Ricardo Rinco <ricardo.rinco@hotmail.com>
2017-08-13 00:25:19 -0300

    Incluido a configuração das rotas com ui router

---
+++ angular1/app/config/routes.js
@@ -0,0 +1,15 @@
+angular.module('financeApp').config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('dashboard', {
+            url: '/dashboard',
+            templateUrl: 'dashboard/dashboard.html'
+        }).state('billingCycle', {
+            url: '/billingCycles',
+            templateUrl: 'billingCycle/tabs.html'
+        });
+
+        $urlRouterProvider.otherwise('/dashboard');
+    }
+]);
