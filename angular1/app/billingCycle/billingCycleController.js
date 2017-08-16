714a271ac0304d836fd040485291f4ea167829bd
Ricardo Rinco <ricardo.rinco@hotmail.com>
2017-08-16 10:10:27 -0300

    Ajustes no sumário de créditos e débitos

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -14,6 +14,8 @@
             $http.get(url).then(function (response) {
                 vm.billingCycle = { credits: [{}], debts: [{}] };
                 vm.billingCycles = response.data;
+                vm.calculateValues();
+
                 tabs.show(vm, { tabList: true, tabCreate: true });
             });
         };
@@ -49,11 +51,13 @@
 
         vm.showTabUpdate = function (billingCycle) {
             vm.billingCycle = billingCycle;
+            vm.calculateValues();
             tabs.show(vm, { tabUpdate: true });
         };
 
         vm.showTabDelete = function (billingCycle) {
             vm.billingCycle = billingCycle;
+            vm.calculateValues();
             tabs.show(vm, { tabDelete: true });
         };
 
@@ -63,11 +67,13 @@
 
         vm.cloneCredit = function (index, { name, value }) {
             vm.billingCycle.credits.splice(index + 1, 0, { name, value });
+            vm.calculateValues();
         };
 
         vm.deleteCredit = function (index) {
             if (vm.billingCycle.credits.length > 1) {
                 vm.billingCycle.credits.splice(index, 1);
+                vm.calculateValues();
             }
         };
 
@@ -77,12 +83,31 @@
 
         vm.cloneDebt = function (index, { name, value }) {
             vm.billingCycle.debts.splice(index + 1, 0, { name, value });
+            vm.calculateValues();
         };
 
-        vm.deleteDebt = function(index) {
+        vm.deleteDebt = function (index) {
             if (vm.billingCycle.debts.length > 0) {
                 vm.billingCycle.debts.splice(index, 1);
+                vm.calculateValues();
             }
+        };
+
+        vm.calculateValues = function () {
+            vm.credit = 0;
+            vm.debt = 0;
+
+            if (vm.billingCycle) {
+                vm.billingCycle.credits.forEach(function ({ value }) {
+                    vm.credit += !value || isNaN(value) ? 0 : parseFloat(value);
+                });
+
+                vm.billingCycle.debts.forEach(function ({ value }) {
+                    vm.debt += !value || isNaN(value) ? 0 : parseFloat(value);
+                });
+            }
+
+            vm.total = vm.credit - vm.debt;
         };
 
         vm.refresh();
